4e33401c94882afcd790e380e5c5b857f7aee1ee
Felix Riddle <fedecraft3312@gmail.com>
2025-12-01 17:20:24 -0300

    Node package class

---
+++ src/NodePackage.ts
@@ -0,0 +1,66 @@
+import path from "path";
+import fsp from "fs/promises";
+
+import { appPackageJson } from "./apps";
+import { promisifiedSpawn } from "./cmd";
+
+/**
+ * Node package
+ */
+export default class NodePackage {
+	path: string;
+	packageJson: any;
+	packageName: string;
+	name: string;
+
+	constructor(appPath: string, packageJson: any) {
+		this.path = appPath;
+		this.packageJson = packageJson;
+		this.packageName = packageJson["name"];
+		this.name = path.basename(appPath);
+	}
+
+	/**
+	 * Create from a given path
+	 */
+	static async fromPath(appPath: string) {
+		const packageJson = await appPackageJson(appPath);
+		return new NodePackage(appPath, packageJson);
+	}
+
+	/**
+	 * Save package json modifications
+	 */
+	async save() {
+		const packageJsonPath = path.join(this.path, "package.json");
+		await fsp.writeFile(
+			packageJsonPath,
+			JSON.stringify(this.packageJson, null, 4)
+		);
+	}
+
+	/**
+	 * Run npm install on the package
+	 */
+	async install() {
+		return await promisifiedSpawn("npm", ["install"], { cwd: this.path });
+	}
+
+	/**
+	 * Run npm build
+	 */
+	async build() {
+		return await promisifiedSpawn("npm", ["run", "build"], {
+			cwd: this.path,
+		});
+	}
+
+	/**
+	 * Run npm publish command
+	 */
+	async publish() {
+		return await promisifiedSpawn("npm", ["publish"], {
+			cwd: this.path,
+		});
+	}
+}
---
+++ src/cmd.ts
@@ -0,0 +1,44 @@
+import { spawn } from "child_process";
+
+/**
+ * Promisified spawn
+ *
+ * @returns
+ */
+export function promisifiedSpawn(
+	command: string,
+	args: string[] = [],
+	options: {
+		cwd?: string;
+	} = {}
+) {
+	return new Promise<number>((resolve, reject) => {
+		// 'spawn' is often preferred as it doesn't use a shell by default (safer),
+		// and handles large output by streaming.
+		const child = spawn(command, args, {
+			stdio: "inherit",
+			// Expand given options
+			...(options ? options : {}),
+		});
+
+		// stdio: 'inherit' pipes the child's stdin/stdout/stderr directly to the parent,
+		// which is often desired for setup scripts or interactive tools.
+
+		// Reject promise on error
+		child.on("error", (err) => {
+			console.error(`Failed to start subprocess: ${err}`);
+			reject(err);
+		});
+
+		// The 'close' event is emitted when the stdio streams of a child process have been closed.
+		child.on("close", (code) => {
+			// 0 exit code means success, therefore we resolve with that
+			if (code === 0) {
+				resolve(code);
+			} else {
+				// Reject the promise if the exit code is not 0
+				reject(new Error(`Command exited with code ${code}`));
+			}
+		});
+	});
+}
